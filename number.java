d20faf9f7df5a4e02928c5bddb5252e5f872e600
hyndavichowdary <41735846+hyndavichowdary@users.noreply.github.com>
2018-08-13 11:52:14 +0530

    Create number.java

---
+++ number.java
@@ -0,0 +1,9 @@
+int num = 100, sum = 0;
+
+        for(int i = 1; i <= num; ++i)
+        {
+            // sum = sum + i;
+            sum += i;
+        }
+
+        System.out.println("Sum = " + sum);
